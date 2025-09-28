ef8892d81a515dd52ea71fe18ded837cf68720af
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2025-09-28 21:54:09 +0200

    Update appEjercicio1.js

--- UD4/04-EjerciciosArrays/js/appEjercicio1.js
+++ UD4/04-EjerciciosArrays/js/appEjercicio1.js
@@ -16,9 +16,7 @@ const aNumeros = [];
  * @description rellenar el array con números entre 500 y 1000
  */
 const rellenarArray = () => { //rellenar el array
-  for (let fila = 0; fila < 20; fila++) {
-    aNumeros.push(Math.floor(Math.random() * (1500 - (-500)+1) + -500));
-  }
+ 
 };
 
 /**
@@ -26,15 +24,7 @@ const rellenarArray = () => { //rellenar el array
  * @param {string} texto será el mensaje a visualizar
  */
 const mostrarArray = (texto) => {
-  let resultado="";
-  document.writeln(`<br>---${texto}---<br>`);
- 
-  aNumeros.forEach((element) => {
-    resultado+=`${element}, `
-  });
-  const posicionUlt=resultado.lastIndexOf(',');
   
-  document.write(resultado.slice(0,posicionUlt));
 };
 
 /**
@@ -42,19 +32,7 @@ const mostrarArray = (texto) => {
  * @returns {string} retorna mensajes
  */
 const mostrarDatos = () => {
-  let resultado='';
-  //Utiliza el método find para encontrar la primera transacción con un gasto
-  const gasto = aNumeros.find(gasto => gasto < 0);
-  resultado+=`<br>El primer gasto es ${gasto}<br>`;
-  //Utiliza el método some para verificar si hay alguna transacción con un ingreso mayor de 900
-  const ingresoNovecientos = aNumeros.some(ingreso => ingreso > 900);
-  resultado+=`¿Hay ingresos mayores a 900? ${ingresoNovecientos?'Sí':'No'}<br>`;
-
-  //Utiliza el método reduce para calcular el saldo total después de todas las transacciones.
-  const saldoTotal = aNumeros.reduce((saldo, operacion) =>saldo+operacion,0);
-  resultado+=`Saldo total= ${saldoTotal}€<br>`;
-  aNumeros.sort((a,b)=>b-a); //ordenar descendentemente
-  return resultado
+  
 };
 
 //script
