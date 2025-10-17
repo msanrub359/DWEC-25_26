c6b92ca1331884a215ed2688948870a20b2df920
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2025-10-17 12:59:55 +0200

    Update appObserver.js

--- UD4/12-PatronesDiseño/js/appObserver.js
+++ UD4/12-PatronesDiseño/js/appObserver.js
@@ -1,69 +1,120 @@
 "use strict"
+//Patrón de Modulo
+const SensorModule = (()=>{
+    //clase Sensor (Sujeto). Emisor de notificaciones. Guarda una lista de observdores y les notifica cuando hay nuevos datos
+    class Sensor {
+        #id;
+        #observers;
+        constructor(id){
+            this.#id = id;
+            this.#observers =[];
+        }
 
-/**
- * @description Patrón de Diseño Observer. Supongamos que estamos desarrollando un sistema
- *  de chat en tiempo real. Queremos que los usuarios puedan enviar mensajes
- *  y que esos mensajes se muestren automáticamente en tiempo real en la interfaz de usuario 
- * de todos los demás usuarios conectados.
- * 
- */
+        get id(){
+            return this.#id;
+        }
+        addObserver(observer){
+            if (!this.#observers.includes(observer)){
+                this.#observers.push(observer)
+            }
+        }
+        removeObserver(observer){
+            this.#observers=this.#observers.filter(observador=> observador!=observer);
+        }
+        notificar (data){
+            this.#observers.forEach(observador =>{
+                observador.update(data)
+            })
+        }
+        
+    }
+    // crear la clase del DigitalSensor que hereda de Sensor
+    class DigitalSensor extends Sensor{
+        #tipo;
+        constructor(id){
+            super(id);
+            this.#tipo = "digital"
+        }  
+        lectura(){
+            //simular la temperatura
+            const temp=(Math.random()* (30-20+1))+20
+            //llamar al método notificar de la clase base Sensor
+            this.notificar({id:this.id, tipo:this.#tipo, value:temp.toFixed(2)})
+        }  
 
- //Chat es el sujeto. El objeto que está observado
- //Clase tiene la lista de suscriptores y les notifica el cambio
-class Chat {
-  #mensajes;
-  #usuarios;
-  constructor() {
-    this.#mensajes = []; //histórico de mensajes
-    this.#usuarios= []; //usuarios
-  }
+    }
+    // crear la clase del AnalogSensor que hereda de Sensor
+    class AnalogSensor extends Sensor{
+        #tipo;
+        constructor(id){
+            super(id);
+            this.#tipo = "analog"
+        }  
+        lectura(){
+            //simular la temperatura
+            const temp=(Math.random()* (30-15+1))+15
+            //llamar al método notificar de la clase base Sensor
+            this.notificar({id:this.id, tipo:this.#tipo, value:temp.toFixed(2)})
+        }  
 
-  addUsuario(usuario) {
-    this.#usuarios.push(usuario);
-  }
+    }
+    //clase SensorFactory
+    class SensorFactory{
+        static crearSensor(tipo, id){
+            switch (tipo.toLocaleLowerCase()){
+                case 'digital':
+                    return new DigitalSensor(id);
+                case 'analog':
+                    return new AnalogSensor(id);
+            }
+        }
+    }
+    //Crear la clase observador Monitor. Receptor de las notificaciones
+    class Monitor{
+        #sensors;
+        constructor (){ //Singlenton
+            if (Monitor.instance){
+                return Monitor.instance
+            }
+            this.#sensors =[];
+            Monitor.instance=this;
+        }
+        addSensor(sensor){
+           this.#sensors.push(sensor) ;
+           sensor.addObserver(this);
+        }
+        delSensor(sensor){
+            this.#sensors=this.#sensors.filter(sen=>!sen !==sensor);
+            sensor.removeObserver(this);
+        }
+        //Método de notificaciones. Es llamado automáticamente por los sensores
+        update(data){
+            console.log(`Monitor recibió datos: Sensor: ${data.id}, tipo: ${data.tipo}, temperatura: ${data.value}`);
+        }
+    }
+    return{
+        Monitor,
+        SensorFactory
+    }
 
-  removeUsuario(usuario) {
-    // const index = this.#usuarios.findIndex(usu=usu==usuario);
-    // if (index !== -1) {
-    //   this.#usuarios.splice(index, 1);
-    // };
-     this.#usuarios = this.#usuarios.filter(user => user !=usuario);
-  }
+})()
 
-  sendMensaje(mensaje) {
-    this.#mensajes.push(mensaje);
-    this.notificarUsuarios(mensaje);
-  }
+//app principal
+//crear el objeto monitor (observador)
+const  monitor = new SensorModule.Monitor();
+//crear sensores
+const sensor1 = SensorModule.SensorFactory.crearSensor("digital", 1);
+const sensor2 = SensorModule.SensorFactory.crearSensor("analog", 2);
+const sensor3 = SensorModule.SensorFactory.crearSensor("digital", 3);
 
-  notificarUsuarios(mensaje) {
-    this.#usuarios.forEach((usuario) => {
-      usuario.update(mensaje);
-    });
-  }
-}
-//ChatUser es el observador. Reacciona ante los cambios
-class ChatUser {
-  #usuario;
-  constructor(usuario) {
-    this.#usuario = usuario;
-  }
-
-  update(mensaje) {
-    console.log(`${this.#usuario} ha recibido un nuevo mensaje: ${mensaje}`);
-  }
-}
-
-// Ejemplo de uso
-const chat = new Chat();
-
-const user1 = new ChatUser("Usuario1");
-const user2= new ChatUser("Usuario2");
-
-chat.addUsuario(user1);
-chat.addUsuario(user2);
-
-chat.sendMensaje("¡Hola a todos!"); // Salida: Usuario1 ha recibido un nuevo mensaje: ¡Hola a todos!
-                                            //         Usuario2 ha recibido un nuevo mensaje: ¡Hola a todos!
-
-chat.sendMensaje("¿Cómo están?"); // Salida: Usuario1 ha recibido un nuevo mensaje: ¿Cómo están?
-                                          
+//suscribir sensores al monitor
+monitor.addSensor(sensor1);
+monitor.addSensor(sensor2);
+monitor.addSensor(sensor3);
+console.log(monitor);
+//Simular las lecturas de los sensores
+setInterval(()=>{
+    sensor1.lectura();
+    sensor2.lectura();
+    sensor3.lectura();
+})
